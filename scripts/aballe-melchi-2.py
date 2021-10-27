2f44578bfb51bd6d9fe5290535375e55b4220d89
Melchi Aballe <melchiaballe@gmail.com>
2021-10-27 23:24:36 +0800

    created number manipulation

---
+++ scripts/aballe-melchi-2.py
@@ -0,0 +1,15 @@
+def convertStringToInt(a):
+	print ("string a =", a)
+	b = int(a)
+	return b
+
+def convertStringToFloat(a):
+	print ("string a =", a)
+	b = float(a)
+	return b
+
+
+print (convertStringToInt("5"))
+print (" ")
+print (convertStringToFloat("5"))
+print (" ")
