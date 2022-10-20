99a31c486133009c94793f7c43cbd128992bd227
Melchi Lloyd S. Aballe <mlsaballe@Melchis-MBP.phub.net.cable.rogers.com>
2022-10-19 23:30:25 -0400

    Printing with C

---
+++ 2k22/scripts/aballe-melchi-1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("Workshop 1 Part-2\n"
+	"=================\n\n"
+	"Using tab specifiers...\n"
+	"\tTab-1\tTab-2\tTab-3\n" 
+	"\t-----\t=====\t-----\n\n"
+	"\\ this is a back-slash character!\n"
+	"%% this is a percent sign character!\n"
+	"\" this is a double-quote character!\n\n"
+	"My favourite quotes are:\n"
+	"1.\t\"Skill is only developed by hours and hours of work.\"\n"
+	"\t\t\t\t\t\t-Usain Bolt\n\n"
+	"2.\t\"It's not about having time. It's about making time.\"\n"
+	"\t\t\t\t\t\t-unknown\n\n"
+	"3.\t\"All of us do not have equal talent. But, all of us\n"
+	"\thave an equal opportunity to develop our talents.\"\n"
+	"\t\t\t\t\t\t-A.P.J. Abdul Kalam\n\n"
+	);
+
+	return 0;
+}
